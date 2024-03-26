7a2c27df3c1031fc5baba3a0d2a10fdf7b949a5f
Brian-Wagura <briankiraguri@gmail.com>
2024-03-26 08:18:05 +0300

    Minor corrections

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,78 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""
+This module defines the BaseModel class.
+"""
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+
+    """
+    BaseModel class definition
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """
+        Initializes a new instance of BaseModel.
 
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if not kwargs and not hasattr(self, 'skip_save_to_storage'):
+            self.save_to_storage()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
-            models.storage.new(self)
+            self.save_to_storage()
 
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
+    def save_to_storage(self):
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        Saves the instance to the storage.
+        """
+        from models import storage
+        if not hasattr(storage, 'new') or not hasattr(storage, 'save'):
+            raise ImportError("Storage object does not required methods")
+        storage.new(self)
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+        """
+        Returns the string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Saves the instance.
+        """
+        self.updated_at = datetime.now()
+        from models import storage
+        storage.save()
+
+    def to_dict(self):
+        """
+        Converts the instance to a dictionary.
+
+        Returns:
+            Dictionary representing the instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
